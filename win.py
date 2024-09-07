a27cd8d1d5cd3172ffa34928bf6489b1ac2b859a
Joseph Taylor <taylorjoseph3333@gmail.com>
2024-09-07 15:17:52 +0100

    Create rounds

--- win.py
+++ win.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from copy import deepcopy
+import random
 
 class Rank():
     _VALUES = {
@@ -63,11 +65,45 @@ class Card():
     def __repr__(self):
         return self.__str__()
 
-deck = []
-for suit in Suit:
-    for rank in [Rank(r) for r in Rank.ALL_RANKS]:
-        deck.append(Card(suit, rank))
+class DeckType(Enum):
+    STANDARD = 0
 
-print(len(deck))
-print(deck)
+class Run():
+    def makeStandardDeck(self):
+        deck = []
+        for suit in Suit:
+            for rank in [Rank(r) for r in Rank.ALL_RANKS]:
+                deck.append(Card(suit, rank))
+        return deck
+
+    def __init__(self, decktype):
+        self.hands = 4
+        self.discards = 3
+        self.hand_size = 7
+        match decktype:
+            case DeckType.STANDARD:
+                self.deck = self.makeStandardDeck()
+                
+
+class Round():
+    def __init__(self, run, quota):
+        self.quota = quota
+        self.score = 0
+        self.hands = run.hands
+        self.discards = run.discards
+        self.hand_size = run.hand_size
+        self.draw_pile = deepcopy(run.deck)
+        random.shuffle(self.draw_pile)
+        self.discard_pile = []
+        self.hand = []
+        self.draw()
+
+    def draw(self):
+        while len(self.draw_pile) > 0 and len(self.hand) < self.hand_size:
+            self.hand.append(self.draw_pile.pop(0))
+
+
+run = Run(DeckType.STANDARD)
+round = Round(run, 300)
 print("Hi")
+
